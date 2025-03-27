f59ce52a22aca22a78c4e679b47d576330efe654
Samuel A <143317806+SamuelA539@users.noreply.github.com>
2025-03-26 21:11:57 -0400

    Create DB Queries.sql
    Created Queries

---
+++ Database/DB Queries.sql
@@ -0,0 +1,24 @@
+--returns office addresses
+select Office_address from hotelchain HC;
+
+--returns cities with hotesl
+select city from hotel;
+
+--returns hotel rooms under 500
+select * from hotel natural join room where room.hotel_ID = hotel.hotelid;
+select addressline1, city, country, room_number, price, capacity from hotel 
+	natural join room where 
+		room.price <500 and 
+		room.hotel_ID = hotel.hotelid;
+
+--Nested 
+  --returns hotels from hotel chains with less that 75 hotels
+select * from hotel H where H.hotelchain_ID in 
+	(select ChainID from hotelchain where num_of_hotels< 75);
+
+--Aggragate
+  --returns city and address with average hotel room price 
+select addressline1, city, Avg(room.price) as avg_price from hotel, room --, hotelChain
+	where hotel.hotelID = room.hotel_id and hotel.hotelchain_id in (select chainID from hotelchain)
+	--where (hotel.hotelID = room.hotel_id and hotel.hotelchain_id = hotelchain.chainID)
+	group by hotel.city, hotel.addressline1;
