a1731f980a5c0f20d9b6cf2331129da2efa476b1
Samuel A <143317806+SamuelA539@users.noreply.github.com>
2025-03-26 21:16:06 -0400

    Update DB Queries.sql

--- Database/DB Queries.sql
+++ Database/DB Queries.sql
@@ -2,11 +2,16 @@
 select Office_address from hotelchain HC;
 
 --returns cities with hotesl
-select city from hotel;
+select address --city 
+	from hotel;
+
+--returns hotels info and room in single table
+select * from hotel natural join room where room.hotel_ID = hotel.hotelid;
 
 --returns hotel rooms under 500
-select * from hotel natural join room where room.hotel_ID = hotel.hotelid;
-select addressline1, city, country, room_number, price, capacity from hotel 
+select address,
+	--addressline1, city, country, 
+	room_number, price, capacity from hotel 
 	natural join room where 
 		room.price <500 and 
 		room.hotel_ID = hotel.hotelid;
@@ -18,7 +23,10 @@ select * from hotel H where H.hotelchain_ID in
 
 --Aggragate
   --returns city and address with average hotel room price 
-select addressline1, city, Avg(room.price) as avg_price from hotel, room --, hotelChain
+select address, --addressline1, city, 
+	Avg(room.price) as avg_price from hotel, room --, hotelChain
 	where hotel.hotelID = room.hotel_id and hotel.hotelchain_id in (select chainID from hotelchain)
 	--where (hotel.hotelID = room.hotel_id and hotel.hotelchain_id = hotelchain.chainID)
-	group by hotel.city, hotel.addressline1;
+	group by address
+		--hotel.city, hotel.addressline1
+	;
